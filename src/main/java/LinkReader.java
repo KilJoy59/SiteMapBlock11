f18a819a2eb98187acab1cd0fcfc6cf075267423
Eduard Abrakov <abrakovem@mail.ru>
2019-11-21 14:46:30 +0500

    add multithreading

--- src/main/java/LinkReader.java
+++ src/main/java/LinkReader.java
@@ -1,54 +1,112 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 /**
  * Project SiteMap
  * Created by End on окт., 2019
  */
 public class LinkReader {
-    private Set<String> uniqueURL= new HashSet<>();
-    private List<String> domenURL = new ArrayList<>();
-    private String site;
 
-    public LinkReader(String site) {
-        this.site = site;
-    }
+    public static List<Future> listURL = Collections.synchronizedList(new ArrayList<>());
+    public static Set<String> uniqueURL = Collections.synchronizedSet(new HashSet<>());
 
-    public List<String> getDomenURL() {
-        return domenURL;
-    }
 
-    public void getLinks(String url, int level, String space) {
-            try {
-                Document doc = Jsoup.connect(url).ignoreContentType(true).maxBodySize(0).get();
-                Elements links = doc.select("a[href]");
-                if (links.isEmpty()) {
-                    return;
-                }
-                for (Element link : links) {
-                    String this_url = link.absUrl("href").split("#", 2)[0];
-                    boolean add = uniqueURL.add(this_url);
-                    if (add && this_url.contains("skillbox.ru") && this_url.startsWith("https://") && !this_url.contains("www")) {
-                        domenURL.add(space + this_url);
-                        System.out.println("before" + level);
-                        System.out.println(this_url);
-                        getLinks(this_url, level + 1, space + "\t");
-                        System.out.println("after" + level);
-                        System.out.println(this_url);
-                        space = "";
-                        level = 0;
-                    }
-                    if (domenURL.size() == 500) {
-                        break;
-                    }
-                }
-            } catch (IOException ex) {
+    public static void getLinks(String url, ExecutorService executorService) {
+        try {
+            Document doc = Jsoup.connect(url).maxBodySize(0).get();
+            Elements links = doc.select("a[href]");
 
+            if (links.isEmpty()) {
+                return;
             }
+
+            write(url);
+
+            links.stream().map((link) -> link.absUrl("href")).forEachOrdered((this_url) -> {
+
+                if (!uniqueURL.contains(this_url)
+                        && this_url.contains(url)
+                        && !this_url.endsWith(".pdf")
+                        && !this_url.contains("#")
+                        && !this_url.contains("?")
+                        && uniqueURL.add(this_url))
+                {
+                       listURL.add(executorService.submit(new LinkReaderThread(this_url, executorService)));
+                }
+            });
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
+    public static void write(String line) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("siteMap.txt"));
+                bw.write(tabulator(line) + "\n");
+            bw.flush();
+            bw.close();
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static String tabulator(String url) {
+        StringBuffer stringBuffer = new StringBuffer(url);
+        int countOfSlash = 0;
+        for (char element : url.toCharArray()) {
+            if (element == '/')
+                countOfSlash++;
+        }
+        int countOfTabs = countOfSlash - 3;
+        if (countOfTabs > 0) {
+            for (int i = 0; i < countOfTabs; i++) {
+                stringBuffer.insert(0, "\t");
+            }
+        }
+        return stringBuffer.toString();
+    }
+
+   /* public static void getLinks(String url, ExecutorService executorService) {
+        try {
+
+            Document doc = Jsoup.connect(url).maxBodySize(0).get();
+            Elements links = doc.select("a[href]");
+
+            if (links.isEmpty()) {
+                return;
+            }
+
+            links.stream().map((link) -> link.absUrl("href")).forEachOrdered((this_url) -> {
+
+                if (!uniqueURL.contains(this_url)
+                        && this_url.contains(url)
+                        && !this_url.endsWith(".pdf")
+                        && !this_url.contains("#")
+                        && !this_url.contains("?")) {
+                    uniqueURL.add(this_url);
+                    if (!this_url.isEmpty()) {
+                        LinkReaderThread.list.add(executorService.submit(new LinkReaderThread(this_url, executorService)));
+                    }
+                }
+            });
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static List<Future> getTreeURL() {
+        return listURL;
+    }*/
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,48 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
-import javax.print.Doc;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.concurrent.*;
 
 /**
  * Project SiteMap
  * Created by End on окт., 2019
  */
+
 public class Main {
-    public static void main(String[] args) {
-        String my_site = "skillbox.ru";
-        LinkReader obj = new LinkReader(my_site);
-            obj.getLinks("https://skillbox.ru/", 0,"");
-            obj.getDomenURL().forEach(System.out::println);
+
+    private static final String URL = "https://skillbox.ru/";
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int coresCount = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(coresCount);
+        executorService.submit(new LinkReaderThread(URL,executorService));
+
+        shutdownAndAwaitTermination(executorService);
     }
 
+    private static void shutdownAndAwaitTermination(ExecutorService pool) {
+        //отключаем новые задачи которые были отправлены
+        pool.shutdown();
+        try {
+            //ждем завершения существующих задач
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                //отменяем текущие задачи
+                pool.shutdownNow();
+                //ждем реакцию на отмену
+                if (!pool.awaitTermination(60, TimeUnit.SECONDS))
+                    System.err.println("Pool did not terminate");
+            }
+        } catch (InterruptedException ie) {
+            // повторяем
+            pool.shutdownNow();
+            // сохраянем статус
+            Thread.currentThread().interrupt();
+        }
 
     }
+}
+
 
 
